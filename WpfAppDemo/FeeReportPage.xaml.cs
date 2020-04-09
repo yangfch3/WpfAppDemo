00e9856c2e323ed7d30cf1c44df463c08fa69424
yangfch3 <yangfch3@gmail.com>
2020-04-09 14:35:03 +0800

    添加项目文件。

---
+++ WpfAppDemo/FeeReportPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// FeeReportPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class FeeReportPage : Page
+    {
+        public FeeReportPage()
+        {
+            InitializeComponent();
+        }
+
+        // Custom constructor to pass expense report data
+        public FeeReportPage(object data):this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+    }
+}
---
+++ WpfAppDemo/WpfAppDemoHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// WpfAppDemoHome.xaml 的交互逻辑
+    /// </summary>
+    public partial class WpfAppDemoHome : Page
+    {
+        public WpfAppDemoHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            FeeReportPage feeReportPage = new FeeReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(feeReportPage);
+        }
+    }
+}
